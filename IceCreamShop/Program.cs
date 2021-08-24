3af64a38cce823791656f826541034a7a62fafb8
Megan Daubenmier <megan.daubenmier@gmail.com>
2021-08-23 21:18:04 -0500

    Exercises completed without bonus mission.

--- IceCreamShop/Program.cs
+++ IceCreamShop/Program.cs
@@ -11,13 +11,46 @@ namespace IceCreamShop
             List<Flavor> availableFlavors = menu.Flavors;
             List<Cone> availableCones = menu.Cones;
 
-        // TODO: Use a Comparer class to sort the 'flavors' array alphabetically by the 'name'
-        //  field.
+            Console.WriteLine("Flavors - Before: \n");
 
-        // TODO: Use a Comparator class to sort the 'cones' array in increasing order by the 'cost'
-        //  field.
+            foreach (Flavor flavor in availableFlavors)
+            {
+                Console.WriteLine(flavor.Name + "\n");
+            }
 
-        // TODO: Print the 'flavors' and 'cones' lists (in a clear manner) to verify the sorting.
+            Console.WriteLine("Cones - Before: \n");
+
+            foreach (Cone cone in availableCones)
+            {
+                Console.WriteLine(cone.Name + " - " + cone.Cost + "\n");
+            }
+
+            // TODO: Use a Comparer class to sort the 'flavors' array alphabetically by the 'name'
+            //  field.
+
+            FlavorComparer comparer = new FlavorComparer();
+            availableFlavors.Sort(comparer);
+
+            // TODO: Use a Comparator class to sort the 'cones' array in increasing order by the 'cost'
+            //  field.
+
+            availableCones.Sort(new ConeComparer());
+
+            // TODO: Print the 'flavors' and 'cones' lists (in a clear manner) to verify the sorting.
+
+            Console.WriteLine("Flavors - After: \n");
+
+            foreach (Flavor flavor in availableFlavors)
+            {
+                Console.WriteLine(flavor.Name + "\n");
+            }
+
+            Console.WriteLine("Cones - After: \n");
+
+            foreach (Cone cone in availableCones)
+            {
+                Console.WriteLine(cone.Name + " - " + cone.Cost + "\n");
+            }
         }
     }
 }
